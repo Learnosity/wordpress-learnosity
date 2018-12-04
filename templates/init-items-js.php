d22f6383c8c71913a0bb192353447111c781573d
Dmitry Kochnev <dmitry.kochnev@learnosity.com>
2018-12-04 17:24:53 +1100

    [FEATURE] Add onload event to init Items API properly

--- templates/init-items-js.php
+++ templates/init-items-js.php
@@ -1,4 +1,13 @@
 <script>
-	window.learnosityCollection = window.learnosityCollection || [];
-	window.learnosityCollection.push(LearnosityItems.init(<?php echo $signed_request; ?>, { errorListener: function(e) { console.log(e); } }));
+    // WP can add "defer" flag to <script> inside "wp_enqueue_script" function
+    // it might lead to the error that "LearnosityItems" below in undefined
+    // to protect against it, we will use "load" global event to secure that all JS assets are loaded before calling any JS code
+    window.addEventListener("load", function () {
+        window.learnosityCollection = window.learnosityCollection || [];
+        window.learnosityCollection.push(LearnosityItems.init(<?php echo $signed_request; ?>, {
+            errorListener: function (e) {
+                console.log(e);
+            }
+        }));
+    });
 </script>
--- templates/init-reports-js.php
+++ templates/init-reports-js.php
@@ -1,6 +1,13 @@
 <script>
+    // WP can add "defer" flag to <script> inside "wp_enqueue_script" function
+    // it might lead to the error that "LearnosityReports" below in undefined
+    // to protect against it, we will use "load" global event to secure that all JS assets are loaded before calling any JS code
     window.addEventListener("load", function () {
         window.learnosityCollection = window.learnosityCollection || [];
-        window.learnosityCollection.push(LearnosityReports.init(<?php echo $signed_request; ?>, { errorListener: function(e) { console.log(e); } }));
+        window.learnosityCollection.push(LearnosityReports.init(<?php echo $signed_request; ?>, {
+            errorListener: function (e) {
+                console.log(e);
+            }
+        }));
     });
 </script>

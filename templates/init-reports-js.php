2d4f50a35c7f103f10442245eef91f9a75f443b2
Dmitry Kochnev <dmitry.kochnev@learnosity.com>
2018-12-04 17:15:03 +1100

    [FEATURE] add onload event to init reports api

--- templates/init-reports-js.php
+++ templates/init-reports-js.php
@@ -1,4 +1,6 @@
 <script>
-	window.learnosityCollection = window.learnosityCollection || [];
-	window.learnosityCollection.push(LearnosityReports.init(<?php echo $signed_request; ?>, { errorListener: function(e) { console.log(e); } }));
+    window.addEventListener("load", function () {
+        window.learnosityCollection = window.learnosityCollection || [];
+        window.learnosityCollection.push(LearnosityReports.init(<?php echo $signed_request; ?>, { errorListener: function(e) { console.log(e); } }));
+    });
 </script>
